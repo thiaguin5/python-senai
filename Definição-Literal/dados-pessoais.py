ddadf0e3d11a7f8a9b1b5e462611de01673c1b63
Brenosilvxz <josephbreno72@gmail.com>
2026-09-15 15:41:26 -0300

    subindo dados pessoais

--- Definição-Literal/dados-pessoais.py
+++ Definição-Literal/dados-pessoais.py
@@ -1,1 +1,21 @@
 #exercícios dados pessoais
+nome = "Thiago"
+idade = 17
+endereço = "Rua mario andreantine"
+estudante = True 
+
+print (f"ola, meu nome é {nome}")
+print (type(nome))
+
+
+print (f"minha idade é {idade}")
+print (type(idade))
+
+
+print (f"meu endereço é {endereço}")
+print (type(endereço))
+
+
+print (f"Sou estudante ? {estudante}")
+print (type(estudante))
+
