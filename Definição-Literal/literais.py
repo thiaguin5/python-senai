00ad41bdc8b323cce7e2e6781e58854a51295274
Brenosilvxz <josephbreno72@gmail.com>
2026-09-15 14:29:28 -0300

    feat: adicionando literais

---
+++ Definição-Literal/literais.py
@@ -0,0 +1,19 @@
+#String
+nome = "thiago"
+
+
+# Numeros inteiros e decimais 
+idade = 17
+altura = 1.70
+
+
+# Valor lógico (true e false)
+professor = True 
+
+
+# verifica a tipagem das variaveis 
+
+print (type(nome))
+print (type(idade))
+print (type(altura))
+print (type(professor))
