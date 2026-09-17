3b5c04181a5f0cce44eccd1ada4cfb3304e82b9a
Brenosilvxz <josephbreno72@gmail.com>
2026-09-17 14:01:35 -0300

    feat: adicionando input e numeros inteiros

---
+++ Operação-de-entrada-saida/soma-numeros.py
@@ -0,0 +1,20 @@
+#entrada de dados básicos
+numero1 = int (input("digite o primeiro numero: "))
+numero2 = int (input("digite o segundo numero: "))
+
+#processamento computacional
+soma = numero1 + numero2
+subtração = numero1 - numero2
+divisão = numero1 / numero2
+multiplicação = numero1 * numero2
+
+
+#Saida da informação
+print(f"A Soma é: {soma}")
+print(f"A Subtração é: {subtração}")
+print(f"A Divisão é: {divisão}")
+print(f"A Multiplicação é: {multiplicação}")
+
+
+
+
