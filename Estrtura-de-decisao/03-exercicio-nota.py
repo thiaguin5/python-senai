44406f85c8d8f6c93efb8a770794e50745c8d365
Brenosilvxz <josephbreno72@gmail.com>
2026-09-18 14:53:10 -0300

    feat: adicionando verificação if e else

---
+++ Estrtura-de-decisao/03-exercicio-nota.py
@@ -0,0 +1,24 @@
+# Faça um programa em Python que peça a nota de um aluno 
+# e verifique se ele foi aprovado ou reprovado.
+
+# Nota maior ou igual a 6 → Aprovado
+# Nota menor que 6 → Reprovado
+
+
+
+
+#Solicita a nota do aluno 
+
+
+nota = float(input ("qual a nota do aluno: "))
+
+
+
+
+#verifica se foi aprovado
+
+if nota >= 5 :
+    print ("Voce passou de ano")
+else:
+    print ("voce não passou de ano")
+
