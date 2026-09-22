64c5f05b2bf39985779739f0c469d2e61d832326
Brenosilvxz <josephbreno72@gmail.com>
2026-09-22 16:42:18 -0300

    feat: adicionando exercicio de lista agrupada

---
+++ 05-listas/06-lista-agrupadas.py
@@ -0,0 +1,9 @@
+filmes = [
+    ["batman", "Ação"]
+    ["titanic", "romance"]
+    ["capitao america", "tony story"]
+]
+
+print(filmes[0])
+print(filmes [0][0])
+print(filmes[0][1])
---
+++ 05-listas/07-exercicio-lista-comidas.py
@@ -0,0 +1,27 @@
+# Crie uma lista com algumas comidas e seus respectivos tipos.
+
+# Percorra a lista usando um for.
+
+# Se o tipo da comida for "Doce", mostre o nome da comida.
+
+# Resultado:
+
+# Comida doce: Brigadeiro
+# Comida doce: Pudim
+
+comidas = [
+    ["Pizza", "Salgado"],
+    ["Brigadeiro", "Doce"],
+    ["Hambúrguer", "Salgado"],
+    ["Pudim", "Doce"]
+]
+
+for comida in comidas:
+    if comida [1] == "Doce":
+        print("comida Doce", comida [0])
+    
+
+
+
+
+    
